a229e54b88547e0f092ab73a922170f9c0d45491
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-10-18 13:39:22 -0300

    feat(filter-component): criando, estilizando e aplicando componente de filtro

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { FilterBar } from "./components/FilterBar";
 import { ItemForm } from "./components/ItemForm";
 import { ItemList } from "./components/ItemList";
 import { ThemeToggle } from "./components/ThemeToggle";
@@ -6,13 +7,15 @@ import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
   return (
     <ThemeContextProvider>
       <GlobalStyle/>
       <ThemeToggle />
       <ItemProvider>
         <ItemForm />
-        <ItemList />  
+        <FilterBar />
+        <ItemList/>  
       </ItemProvider>
     </ThemeContextProvider>
   )
--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -1,3 +1,47 @@
+
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { FilterBarContainer, SelectWrapper, Button } from './styles';
+import { useContext } from 'react';
+import { ItemContext } from '../../contexts/ItemContext';
+
+const filterSchema = z.object({
+    priority: z.enum(['alta', 'média', 'baixa', 'none']),
+});
+
+type FilterFormData = z.infer<typeof filterSchema>;
+
 export function FilterBar(){
-    return <h1>FilterBar</h1>
+
+    const {filterItems} = useContext(ItemContext);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm<FilterFormData>({
+        resolver: zodResolver(filterSchema),
+      });
+
+    function handleSearchByPriority(data: FilterFormData){
+        //console.log(data);
+        filterItems(data.priority);
+    }
+
+    return (
+        <FilterBarContainer onSubmit={handleSubmit(handleSearchByPriority)}>
+            <SelectWrapper>
+                <select {...register('priority')}>
+                <option value="none">Todas as Tarefas</option>
+                <option value="alta">Alta</option>
+                <option value="média">Média</option>
+                <option value="baixa">Baixa</option>
+                </select>
+                {errors.priority && <p>{errors.priority.message}</p>}
+            </SelectWrapper>
+
+            <Button type="submit">Filtrar</Button>
+        </FilterBarContainer>
+    );
 }
